04dce74fd13fd2fe5a59cf01c8dfafacce731efd
Huseyin <Huseyin_Kizil@etu.u-bourgogne.fr>
2024-05-28 08:30:19 +0200

    code scindé, dernière version

--- buttons.py
+++ buttons.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize, Qt, QRect
 from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QWidget, QSlider
 from PyQt6 import QtGui
 
@@ -140,179 +140,222 @@ def create_left_buttons(main_window):
 def create_right_buttons(main_window):
     right_layout = QVBoxLayout()
 
-    button1 = QPushButton("Get ready", main_window)
-    button1.setIcon(QtGui.QIcon("images/get_ready"))
-    button1.setIconSize(QSize(70, 70))
-    button1.setToolTip("Marty is ready")
-    button1.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button1.clicked.connect(lambda: main_window.on_button_click("Get ready"))
-
-    button2 = QPushButton("Show-off", main_window)
-    button2.setIcon(QtGui.QIcon("images/get_ready"))
-    button2.setIconSize(QSize(70, 70))
-    button2.setToolTip("Marty shows-off")
-    button2.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button2.clicked.connect(lambda: main_window.on_button_click("Show-off"))
-
-
-    button3 = QPushButton("Wave-left", main_window)
-    button3.setIcon(QtGui.QIcon("images/get_ready"))
-    button3.setIconSize(QSize(70, 70))
-    button3.setToolTip("Marty Wave-left")
-    button3.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button3.clicked.connect(lambda: main_window.on_button_click("Wave-left"))
-
-    button4 = QPushButton("Wave-right", main_window)
-    button4.setIcon(QtGui.QIcon("images/get_ready"))
-    button4.setIconSize(QSize(70, 70))
-    button4.setToolTip("Marty Wave-right")
-    button4.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button4.clicked.connect(lambda: main_window.on_button_click("Dance!"))
-
-
     right_layout_line1 = QHBoxLayout()
-    right_layout_line1.addWidget(button1)
-    right_layout_line1.addWidget(button2)
-    right_layout_line1.addWidget(button3)
-    right_layout_line1.addWidget(button4)
 
+    button1_line1_right = QPushButton("Get ready", main_window)
+    button1_line1_right.setGeometry(QRect(300, 300, 211, 23))
+    button1_line1_right.setIcon(QtGui.QIcon("images/get_ready"))
+    button1_line1_right.setIconSize(QSize(70, 70))
+    button1_line1_right.setToolTip("Marty is ready")
+    button1_line1_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button1_line1_right.clicked.connect(lambda: main_window.on_button_click("Get ready"))
+
+    button2_line1_right = QPushButton("Show-off", main_window)
+    button2_line1_right.setIcon(QtGui.QIcon("images/get_ready"))
+    button2_line1_right.setIconSize(QSize(70, 70))
+    button2_line1_right.setToolTip("Marty shows-off")
+    button2_line1_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button2_line1_right.clicked.connect(lambda: main_window.on_button_click("Show-off"))
+
+    button3_line1_right = QPushButton("Wave-left", main_window)
+    button3_line1_right.setIcon(QtGui.QIcon("images/get_ready"))
+    button3_line1_right.setIconSize(QSize(70, 70))
+    button3_line1_right.setToolTip("Marty Wave-left")
+    button3_line1_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button3_line1_right.clicked.connect(lambda: main_window.on_button_click("Wave-left"))
+
+    button4_line1_right = QPushButton("Wave-right", main_window)
+    button4_line1_right.setIcon(QtGui.QIcon("images/get_ready"))
+    button4_line1_right.setIconSize(QSize(70, 70))
+    button4_line1_right.setToolTip("Marty waves-right")
+    button4_line1_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button4_line1_right.clicked.connect(lambda: main_window.on_button_click("Wave-right"))
+
+    right_layout_line1.addWidget(button1_line1_right)
+    right_layout_line1.addWidget(button2_line1_right)
+    right_layout_line1.addWidget(button3_line1_right)
+    right_layout_line1.addWidget(button4_line1_right)
     container_right_line1 = QWidget()
     container_right_line1.setLayout(right_layout_line1)
     right_layout.addWidget(container_right_line1)
 
-    button5 = QPushButton("Dance!", main_window)
-    button5.setIcon(QtGui.QIcon("images/dance"))
-    button5.setIconSize(QSize(70, 70))
-    button5.setToolTip("Marty Dance!")
-    button5.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button5.clicked.connect(lambda: main_window.on_button_click("Dance!"))
-
-    button6 = QPushButton("Wiggle Eyses", main_window)
-    button6.setIcon(QtGui.QIcon("images/wiggle"))
-    button6.setIconSize(QSize(70, 70))
-    button6.setToolTip("Marty Wiggle Eyes")
-    button6.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button6.clicked.connect(lambda: main_window.on_button_click("Wiggle Eyes"))
-
-    button7 = QPushButton("Kick Left", main_window)
-    button7.setIcon(QtGui.QIcon("images/kick_left"))
-    button7.setIconSize(QSize(70, 70))
-    button7.setToolTip("Marty Kick-left")
-    button7.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button7.clicked.connect(lambda: main_window.on_button_click("Kick Left"))
-
-    button8 = QPushButton("Kick Right", main_window)
-    button8.setIcon(QtGui.QIcon("images/kick_right"))
-    button8.setIconSize(QSize(70, 70))
-    button8.setToolTip("Marty Kick right")
-    button8.setStyleSheet(""" 
-        QPushButton {
-            font-size:16px;
-            background-color:"#87CEEB";
-            border: 1px solid black;
-            border-radius: 10px;
-        } 
-        QPushButton:hover {
-            font-size:18px;
-            background-color:"#00FFFF";
-            color: white;
-        }
-    """)
-    button8.clicked.connect(lambda: main_window.on_button_click("Kick right"))
-
-    
     right_layout_line2 = QHBoxLayout()
-    right_layout_line2.addWidget(button5)
-    right_layout_line2.addWidget(button6)
-    right_layout_line2.addWidget(button7)
-    right_layout_line2.addWidget(button8)
+
+    button1_line2_right = QPushButton("Dance!", main_window)
+    button1_line2_right.setIcon(QtGui.QIcon("images/dance"))
+    button1_line2_right.setIconSize(QSize(70, 70))
+    button1_line2_right.setToolTip("Marty's dance")
+    button1_line2_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button1_line2_right.clicked.connect(lambda: main_window.on_button_click("Dance!"))
+
+    button2_line2_right = QPushButton("Wiggle Eyes", main_window)
+    button2_line2_right.setIcon(QtGui.QIcon("images/wiggle"))
+    button2_line2_right.setIconSize(QSize(70, 70))
+    button2_line2_right.setToolTip("Marty wiggle eyes")
+    button2_line2_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button2_line2_right.clicked.connect(lambda: main_window.on_button_click("Wiggle Eyes"))
+
+    button3_line2_right = QPushButton("Kick left", main_window)
+    button3_line2_right.setIcon(QtGui.QIcon("images/kick_left"))
+    button3_line2_right.setIconSize(QSize(70, 70))
+    button3_line2_right.setToolTip("Marty kicks left")
+    button3_line2_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button3_line2_right.clicked.connect(lambda: main_window.on_button_click("Kick left"))
+
+    button4_line2_right = QPushButton("Kick right", main_window)
+    button4_line2_right.setIcon(QtGui.QIcon("images/get_ready"))
+    button4_line2_right.setIconSize(QSize(70, 70))
+    button4_line2_right.setToolTip("Marty kicks right")
+    button4_line2_right.setStyleSheet(""" 
+        QPushButton {
+            font-size:16px;
+            background-color:"#87CEEB";
+            border: 1px solid black;
+            border-radius: 10px;
+        } 
+        QPushButton:hover {
+            font-size:18px;
+            background-color:"#00FFFF";
+            color: white;
+        }
+    """)
+    button4_line2_right.clicked.connect(lambda: main_window.on_button_click("Kick right"))
+
+    right_layout_line2.addWidget(button1_line2_right)
+    right_layout_line2.addWidget(button2_line2_right)
+    right_layout_line2.addWidget(button3_line2_right)
+    right_layout_line2.addWidget(button4_line2_right)
 
     container_right_line2 = QWidget()
     container_right_line2.setLayout(right_layout_line2)
     right_layout.addWidget(container_right_line2)
+    right_layout_line3 = QHBoxLayout()
+
+    slider_label = QLabel("Contrôleur de la vitesse")
+    slider_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    right_layout_line3.addWidget(slider_label)
+
+    main_window.slider = QSlider(Qt.Orientation.Horizontal)
+    main_window.slider.setTickPosition(QSlider.TickPosition.TicksAbove)
+    main_window.slider.setMinimum(0)
+    main_window.slider.setMaximum(100)
+    main_window.slider.setTickInterval(10)
+    main_window.slider.setSingleStep(1)
+    main_window.slider.setValue(50)
+        
+    def update_speed(value):
+        main_window.slider_label.setText(f"Vitesse: {value}")
+
+    main_window.update_speed = update_speed
+    main_window.slider.valueChanged.connect(main_window.update_speed)
+    right_layout.addWidget(slider_label)
+    right_layout.addWidget(main_window.slider)
+    right_layout_line3.addWidget(main_window.slider)
+
+    container_right_line3 = QWidget()
+    container_right_line3.setLayout(right_layout_line3)
+    right_layout.addWidget(container_right_line3)
+
+    right_layout_line4 = QHBoxLayout()
+
+    main_window.slider_label = QLabel("Valeur de la vitesse: 50")
+    right_layout_line4.addWidget(main_window.slider_label)
+    container_right_line4 = QWidget()
+    container_right_line4.setLayout(right_layout_line4)
+    right_layout.addWidget(container_right_line4)
+
+    container_right = QWidget()
+    container_right.setLayout(right_layout)
+
+    right_layout.addWidget(container_right)
+    container = QWidget()
+    container.setLayout(right_layout)
+    # Set the central widget of the Window.
+    main_window.setCentralWidget(container)
+
 
     return right_layout
